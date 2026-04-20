4e5295a9b0387750365f60dc051c0649108dafa5
erensmc <ucar_eren_danyel01@student.smc.edu>
2026-04-20 16:15:54 -0700

    Added assets for prototyping and changed the default data structure.

--- scripts.js
+++ scripts.js
@@ -23,21 +23,58 @@
  *
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
+// All images gotten from wiki-commons
+// All gifs from games created using: https://www.chess.com/gifs
+const players = [
+  {
+    name: "Garry Kasparov",
+    country: "Russia",
+    peakElo: 2851,
+    era: "Modern",
+    famousFor: "Attacking",
+    image: "assets/players/kasparov.jpg",
+    memorableGame: {
+      title: "Kasparov vs. Topalov",
+      year: 1999,
+      event: "Wijk aan Zee",
+      reason: "One of the most famous attacking games in chess history.",
+      previewImage: "assets/games/kasparov.gif",
+      playingAs: "White"
+    }
+  },
+  {
+    name: "Magnus Carlsen",
+    country: "Norway",
+    peakElo: 2882,
+    era: "Modern",
+    famousFor: "Endgames",
+    image: "assets/players/carlsen.jpg",
+    memorableGame: {
+      title: "Carlsen vs. Nepomniachtchi",
+      year: 2021,
+      event: "World Championship",
+      reason: "A 136-move win for Carlsen that lasted 7 hours 45 minutes. As of 2026, it is the longest game in the history of the World Chess Championship.",
+      previewImage: "assets/games/carlsen.gif",
+      playingAs: "White"
+    }
+  },
+  {
+    name: "Bobby Fischer",
+    country: "United States",
+    peakElo: 2785,
+    era: "Modern",
+    famousFor: "Aggresiveness",
+    image: "assets/players/fischer.jpg",
+    memorableGame: {
+      title: "Donald Byrne vs. Fischer",
+      year: 1956,
+      event: "New York",
+      reason: "Fischer played this game at 13years old. Dubbed 'The Game of the Century'",
+      previewImage: "assets/games/fischer.gif",
+      playingAs: "Black"
+    }
+  },
+]
 
 // This function adds cards the page to display the data in the array
 function showCards() {
@@ -45,22 +82,14 @@ function showCards() {
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
+  for (let i = 0; i < players.length; i++) {
+    let title = players[i]["name"];
 
     // This part of the code doesn't scale very well! After you add your
     // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, title, players[i]["image"]); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
@@ -92,6 +121,6 @@ function quoteAlert() {
 }
 
 function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
+  players.pop(); // Remove last item in titles array
   showCards(); // Call showCards again to refresh
 }
