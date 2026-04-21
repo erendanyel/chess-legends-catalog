6fd9704348b82333f3b862ec832eae03ed4ed32b
erensmc <ucar_eren_danyel01@student.smc.edu>
2026-04-21 10:38:59 -0700

    - Add favicon - Enhanced filterd functionality with Elo readout next to slider

--- scripts.js
+++ scripts.js
@@ -1,28 +1,3 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
-
 // All images gotten from wiki-commons
 // All gifs from games created using: https://www.chess.com/gifs
 const players = [
@@ -107,8 +82,10 @@ function populateFilters() {
   eloMaxSlider.max = maxElo;
   eloMaxSlider.min = minElo;
   eloMaxSlider.value = maxElo;
+  updateEloReadout();
 }
 
+// See if the given player passes the current filter
 function filterPass(player)
 {
   const searchField = document.querySelector("#search");
@@ -214,6 +191,12 @@ function flipCards() {
     card.classList.toggle("flipped");
   }
   );
+}
+
+function updateEloReadout()
+{
+  const slider = document.querySelector("#max-elo");
+  document.querySelector("#elo-display").textContent = slider.value;
 }
 
 function removeLastCard() {
